24778090af7136dc73a4f0821514aa37dc45aaf8
Lyn-G <lexicolorful@gmail.com>
2023-11-30 14:46:06 -0800

    added main files

---
+++ main.js
@@ -0,0 +1,13 @@
+import * as Phaser from "phaser";
+
+const config = {
+  width: 640,
+  height: 480,
+  scene: [],
+  backgroundColor: "#6b8e23",
+};
+
+new Phaser.Game(config);
+
+document.title = "Trees vs Hillbillies";
+document.body.style.backgroundColor = "#6F4E37";
